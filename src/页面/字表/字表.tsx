3b143bd33a0a8981e544ea47a9242e294cc82f8d
Jun Yeu Mah <junyeumah@gmail.com>
2020-05-20 00:16:56 +1000

    Only show commonly used characters

--- src/页面/字表/字表.tsx
+++ src/页面/字表/字表.tsx
@@ -2,24 +2,46 @@ import React, { useMemo } from "react";
 import 容器 from "react-bootstrap/Container";
 import { RouteComponentProps, Link } from "react-router-dom";
 import 数据 from "../../数据";
+import 常见 from "../../数据/常见";
 import 主标头 from "../../组件/主标头";
 
 interface RouteParams {
   radical: string;
 }
 
-const 字表: React.FC<RouteComponentProps<RouteParams>> = ({ match }) => {
+const 字表: React.FC<RouteComponentProps<RouteParams>> = ({
+  match,
+  location
+}) => {
   const { radical: 部首 } = match.params;
+  const 显示所有 = new URLSearchParams(location.search).get("all") !== null;
 
   const 部首对象 = useMemo(() => {
     for (let i = 0; i < 数据.length; i++) {
-      const radicalGroup = 数据[i].radicals.find((rad) => {
+      const 部首对象 = 数据[i].radicals.find((rad) => {
         return rad.symbol === 部首;
       });
 
-      if (radicalGroup) return radicalGroup;
+      if (部首对象) {
+        if (显示所有) return 部首对象;
+
+        const 部首对象副本 = {
+          ...部首对象
+        };
+
+        部首对象副本.strokeNumbers = 部首对象副本.strokeNumbers
+          .map((strokeNumber) => ({
+            ...strokeNumber,
+            characters: strokeNumber.characters.filter((char) =>
+              常见(char.character)
+            )
+          }))
+          .filter((strokeNumber) => strokeNumber.characters.length > 0);
+
+        return 部首对象副本;
+      }
     }
-  }, [部首]);
+  }, [部首, 显示所有]);
 
   return (
     <>
@@ -33,7 +55,7 @@ const 字表: React.FC<RouteComponentProps<RouteParams>> = ({ match }) => {
                 <p>画{笔画数.strokeNumber}</p>
                 <ul>
                   {笔画数.characters.map((字) => (
-                    <li>
+                    <li key={字.character}>
                       <strong>
                         <Link to={`/character/${字.character}`}>
                           {字.character}
